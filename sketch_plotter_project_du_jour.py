8dbb8f750e0edfec521c0c8885a32816d574f7ec
R4chel <rae84@cornell.edu>
2023-03-26 17:41:52 -0400

    trying more functions

--- sketch_plotter_project_du_jour.py
+++ sketch_plotter_project_du_jour.py
@@ -5,6 +5,14 @@ import numpy as np
 from point2d import Point2D
 
 
+def f(x, y):
+    # return (x**2 - y**3 + 0.1 * x * x * y, y - x + .2 * x**4)
+    # return (x**2 + y**2 + 3 * x - 2 * x * y + x**2 * y - 0.1 * y**2 * x - x**2,
+    #         2 * x * y * y + .0001 * x**2 - .02 * (y)**3)
+    return (x**2 + y**2 + 3 * x - 2 * x * y + x**2 * y - 0.1 * y**2 * x - x**2,
+            2 * x * y * y + .3 * x**2 - .02 * (y)**3)
+
+
 class PlotterProjectDuJourSketch(vsketch.SketchClass):
     # Sketch parameters:
     debug = vsketch.Param(False)
@@ -33,13 +41,25 @@ class PlotterProjectDuJourSketch(vsketch.SketchClass):
         cy = self.height / 2
         thetas = np.linspace(0, 2 * np.pi, self.num_points)
         circle = [Point2D(a=theta, r=1).cartesian() for theta in thetas]
-        mapping = [(x**2 - y**2 + 3 * x - 2 * x * y,
-                    2 * x * y * y + .0001 * x**2 - .02 * (y)**3)
-                   for (x, y) in circle]
+        # mapping = [(x**2 + y**2 + 3 * x - 2 * x * y - np.sin(x * y) +
+        #             x**2 * y - 0.1 * y**2 * x - x**4,
+        #             2 * x * y * y + .0001 * x**2 - .02 * (y)**3)
+        #            for (x, y) in circle]
+
+        # mapping = [(x * y - x + x**3, 0.1 * x**3 * y**2 - x * y + .3 * y)
+        #            for (x, y) in circle]
+        mapping = [f(x, y) for (x, y) in circle]
+        # mapping = [f(x, y) for (x, y) in mapping]
         shape = LinearRing(mapping)
+
         shape = affinity.scale(shape, self.radius, self.radius)
         shape = affinity.translate(shape, cx, cy)
+        shape2 = affinity.scale(LinearRing(mapping), self.radius / 2,
+                                self.radius / 2)
+        shape2 = affinity.translate(shape2, cx, cy)
         vsk.geometry(shape)
+        # vsk.stroke(2)
+        # vsk.geometry(shape2)
 
     def finalize(self, vsk: vsketch.Vsketch) -> None:
         vsk.vpype("linemerge linesimplify reloop linesort")
