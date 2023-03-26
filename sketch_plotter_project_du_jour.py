aa22b72443259a38b249828199e1dbc7a7908b45
R4chel <rae84@cornell.edu>
2023-03-26 14:48:32 -0400

    draw a circle

--- sketch_plotter_project_du_jour.py
+++ sketch_plotter_project_du_jour.py
@@ -1,5 +1,8 @@
 import vsketch
-from shapely.geometry import Point
+from shapely.geometry import Point, LinearRing
+import numpy as np
+from point2d import Point2D
+
 
 class PlotterProjectDuJourSketch(vsketch.SketchClass):
     # Sketch parameters:
@@ -10,17 +13,26 @@ class PlotterProjectDuJourSketch(vsketch.SketchClass):
     landscape = vsketch.Param(True)
     pen_width = vsketch.Param(0.7, decimals=3, min_value=1e-10, unit="mm")
     num_layers = vsketch.Param(1)
-    # radius = vsketch.Param(1.0, decimals=3, unit="in")
+    num_points = vsketch.Param(360)
+    radius = vsketch.Param(1.0, decimals=3, unit="in")
 
     def random_point(self, vsk: vsketch.Vsketch):
         return Point(vsk.random(0, self.width), vsk.random(0, self.height))
 
     def draw(self, vsk: vsketch.Vsketch) -> None:
-        vsk.size(f"{self.height}x{self.width}", landscape=self.landscape, center=False)
+        vsk.size(f"{self.height}x{self.width}",
+                 landscape=self.landscape,
+                 center=False)
         self.width = self.width - 2 * self.margin
         self.height = self.height - 2 * self.margin
         vsk.translate(self.margin, self.margin)
         vsk.penWidth(f"{self.pen_width}")
+
+        center = Point2D(self.width / 2, self.height / 2)
+        thetas = np.linspace(0, 2 * np.pi, self.num_points)
+        points2d = [Point2D(a=theta, r=self.radius) for theta in thetas]
+        vsk.geometry(
+            LinearRing([Point((p + center).cartesian()) for p in points2d]))
 
         # implement your sketch here
         # layers = [1 + i for i in range(self.num_layers)]
